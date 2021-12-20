d9de10843f6ef28fc1cfe30fb7620a731d57f718
jitasek <jitka.moreira@gmail.com>
2021-12-20 12:35:58 +0100

    add route get all categories

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const { sequelize } = require("./models/Product");
+const routes = require("./routes");
+require("dotenv").config();
 // import sequelize connection
+const connection = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
