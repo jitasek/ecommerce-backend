984976f4228f2116f37039c11234fedcc9ac4fa8
jitasek <jitka.moreira@gmail.com>
2021-12-20 19:19:21 +0100

    add routes delete, update tag by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,12 +36,41 @@ router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tags = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    console.info(`[ERROR]: ${err.message}`);
+    res.status(500).json({
+      error: "No tag found to update with this ID.",
+    });
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tags = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tags) {
+      res.status(404).json({
+        message: "No tag found with this ID.",
+      });
+      return;
+    }
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
